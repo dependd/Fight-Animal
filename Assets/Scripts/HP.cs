b8fa5913e04014ba8628f3cde867ba382944b27d
松田幸介 <darby8928@gmail.com>
2018-04-28 13:26:15 +0900

    menber用のフォルダーを作成

---
+++ Assets/Scripts/HP.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour {
+    Slider _enemyslider;
+    Slider _partyslider;
+	// Use this for initialization
+	void Start () {
+        //スライダーを取得
+        _enemyslider = GameObject.Find("enemySlider").GetComponent<Slider>();
+
+        _partyslider = GameObject.Find("partySlider").GetComponent<Slider>();
+    }
+    //HPの値
+    float hp = 100;
+    //HPを減らす
+    // Update is called once per frame
+    void Update()
+    {
+        hp -= 1;
+        //HPゲージに値を設定
+        _enemyslider.value = hp;
+
+        _partyslider.value = hp;
+
+    }
+}
---
+++ Assets/Scripts/start.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour {
+
+    
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TransitionToResult();
+        }
+	}
+    public void TransitionToResult()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+}

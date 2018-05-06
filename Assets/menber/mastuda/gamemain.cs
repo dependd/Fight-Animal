fb8ca3e6effa476c16d305be4c73472df01fc2bd
松田幸介 <darby8928@gmail.com>
2018-05-06 21:38:22 +0900

    敵の攻撃に対してもノーツを消す処理を追加

--- Assets/menber/mastuda/gamemain.cs
+++ Assets/menber/mastuda/gamemain.cs
@@ -13,6 +13,10 @@ public class gamemain : MonoBehaviour
     GameObject note2;
     GameObject note3;
     GameObject note4;
+    GameObject enemynote1;
+    GameObject enemynote2;
+    GameObject enemynote3;
+    GameObject enemynote4;
     //メニューを押したら(一応)スタートに戻る
     public void menuButton()
     {
@@ -25,6 +29,10 @@ public class gamemain : MonoBehaviour
         note2 = GameObject.Find("note2");
         note3 = GameObject.Find("note3");
         note4 = GameObject.Find("note4");
+        enemynote1 = GameObject.Find("enemynote1");
+        enemynote2 = GameObject.Find("enemynote2");
+        enemynote3 = GameObject.Find("enemynote3");
+        enemynote4 = GameObject.Find("enemynote4");
     }
 
     void Update()
@@ -43,14 +51,33 @@ public class gamemain : MonoBehaviour
             {
                 if (mouseClickPosition.y >= 190.0 && mouseClickPosition.y <= 676.0)
                 {
-                    Debug.Log("敵の攻撃を防いだ");
+                    if (enemynote1.transform.position.x <= -3 && enemynote1.transform.position.x >= -5)
+                    {
+                        enemynote1.transform.position = new Vector2(-7.5f, 5);
+                        Debug.Log("敵の攻撃を防いだ");
+                    }
+                    if (enemynote2.transform.position.x <= -3 && enemynote2.transform.position.x >= -5)
+                    {
+                        enemynote2.transform.position = new Vector2(-7.5f, 5);
+                        Debug.Log("敵の攻撃を防いだ");
+                    }
+                    if (enemynote3.transform.position.x <= -3 && enemynote3.transform.position.x >= -5)
+                    {
+                        enemynote3.transform.position = new Vector2(-7.5f, 5);
+                        Debug.Log("敵の攻撃を防いだ");
+                    }
+                    if (enemynote4.transform.position.x <= -3 && enemynote4.transform.position.x >= -5)
+                    {
+                        enemynote4.transform.position = new Vector2(-7.5f, 5);
+                        Debug.Log("敵の攻撃を防いだ");
+                    }
                 }
             }
             if (mouseClickPosition.x >= 780.0 && mouseClickPosition.x <= 1024.0f)
             {
                 if (mouseClickPosition.y >= 559.0 && mouseClickPosition.y <= 676.0)
                 {
-                    if (note1.transform.position.x >= 3 && note1.transform.position.x <= 4)
+                    if (note1.transform.position.x >= 3 && note1.transform.position.x <= 5)
                     {
                         note1.transform.position = new Vector2(-10, 5);
                         Debug.Log("勇者１攻撃");
@@ -61,7 +88,7 @@ public class gamemain : MonoBehaviour
             {
                 if (mouseClickPosition.y >= 451.0 && mouseClickPosition.y < 559.0)
                 {
-                    if (note2.transform.position.x >= 3 && note2.transform.position.x <= 4)
+                    if (note2.transform.position.x >= 3 && note2.transform.position.x <= 5)
                     {
                         note2.transform.position = new Vector2(-10, 5);
                         Debug.Log("勇者2攻撃");
@@ -72,7 +99,7 @@ public class gamemain : MonoBehaviour
             {
                 if (mouseClickPosition.y >= 322.0 && mouseClickPosition.y <= 451.0)
                 {
-                    if (note3.transform.position.x >= 3 && note3.transform.position.x <= 4)
+                    if (note3.transform.position.x >= 3 && note3.transform.position.x <= 5)
                     {
                         note3.transform.position = new Vector2(-10, 5);
                         Debug.Log("勇者3攻撃");
@@ -83,7 +110,7 @@ public class gamemain : MonoBehaviour
             {
                 if (mouseClickPosition.y >= 190.0 && mouseClickPosition.y <= 322.0)
                 {
-                    if (note4.transform.position.x >= 3 && note4.transform.position.x <= 4)
+                    if (note4.transform.position.x >= 3 && note4.transform.position.x <= 5)
                     {
                         note4.transform.position = new Vector2(-10, 5);
                         Debug.Log("勇者4攻撃");

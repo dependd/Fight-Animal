a8e84c66a12b2e9c389ae1582b92dbb497ca68d0
nojima <17d3170020nm@edu.tech.ac.jp>
2018-05-13 16:21:35 +0900

    menu制作途中

---
+++ Assets/menber/nojima/menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour {
+
+
+    public void MenuButton() {
+        if (Input.GetButtonDown(buttonName: "Button")){
+            if (Time.timeScale == 1.0F)
+                Time.timeScale = 0F;
+            
+        }
+        Debug.Log("click");
+       
+    }
+
+}

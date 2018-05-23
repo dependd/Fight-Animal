043b2843ed35bae966ba7a87158b37c2a3d6bcbf
野島 <17d3170020nm@edu.tech.ac.jp>
2018-05-23 18:51:02 +0900

    HPcolorの編集

--- Assets/menber/nojima/scripts/HP/HPcolor.cs
+++ Assets/menber/nojima/scripts/HP/HPcolor.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class HPcolor : MonoBehaviour {
-    /*
+    
     //変数を入れる箱を用意
     float EnemyHp;
     float PartyHp;
     private GameObject enemySlider;
     private GameObject FillColor1;
     private GameObject FillColor2;
-    value hp;
+    hp hp;
 
     //色の指定はfloat型のRGBA値に255.0fで除算したもの
+
+    private Color Green = Color.green;
     private Color Red = new Color(255.0f / 255.0f, 0.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f);
     private Color Orage = new Color(255.0f / 255.0f ,110.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f);
-
+    //private Color Green = new Color(0.0f / 255.0f , 255.0f / 255.0f ,110.0f / 255.0f);
 
     void Start(){
         //GameObjectを取得
@@ -24,8 +26,8 @@ public class HPcolor : MonoBehaviour {
         FillColor1 = GameObject.Find("Fill");
         FillColor2 = GameObject.Find("Fill2");
         //hp内のenemyhpとpartyhpを取得
-         EnemyHp = enemySlider.GetComponent<value>().enemyhp;
-         PartyHp = enemySlider.GetComponent<value>().partyhp;
+         EnemyHp = enemySlider.GetComponent<hp>().enemyhp;
+         PartyHp = enemySlider.GetComponent<hp>().partyhp;
     }
 
     
@@ -44,11 +46,12 @@ public class HPcolor : MonoBehaviour {
             enemySlider.GetComponent<Image>().color = Red;
         }
 
-        if (PartyHp >= 160){
+        if (PartyHp >= 160) {
+            FillColor2.GetComponent<Image>().color = Green;
+        } else if (PartyHp < 160 && PartyHp >= 80) {
             FillColor2.GetComponent<Image>().color = Orage;
+        }else if (PartyHp < 80) {
+            FillColor2.GetComponent<Image>().color = Red;
         }
-        else if (PartyHp < 80){
-            FillColor2.GetComponent<Image>().color = Orage;
-        }
-    }*/
+    }
 }

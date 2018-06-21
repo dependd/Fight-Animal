a7a14f88829315f5f893549b454227b7651322bb
nojima <17d3170020nm@edu.tech.ac.jp>
2018-06-22 04:19:31 +0900

    メニュー画面の作成

--- Assets/menber/nojima/scripts/menu.cs
+++ Assets/menber/nojima/scripts/menu.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class menu : MonoBehaviour {
+
+    bool stop;
+    public GameObject menuScreen;
     //Time.timescaleはUpdateは動いてFixedUpdateは動かない。
     //Updateで動いているものをFixedUpdateに変えてもらう
     public void MenuButton() {
             if (Time.timeScale == 1){
                 Time.timeScale = 0;
+            stop = true;
             }
+            else if (Time.timeScale == 0){
+                Time.timeScale = 1;
+            stop = false;
+            }
+    }
+    private void Start(){
+        stop = false;
+        menuScreen.SetActive(false);
+    }
+
+    public void MenuScreen() {
+        if (stop == true){
+            menuScreen.SetActive(true);
+        }else if (stop == false) {
+            menuScreen.SetActive(false);
         }
-        
-       
-    
+    }
+    public void retrun() {
+        SceneManager.LoadScene("start");
+    }
+
+
+
 
 }

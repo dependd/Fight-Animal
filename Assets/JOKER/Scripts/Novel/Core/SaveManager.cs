fe2e7be8c06bcfcfa363197b25280848b8f277b6
高田将志 <masapo.0315@icloud.com>
2018-07-02 08:23:23 +0900

    再びジョーカースクリプトをインポート、シナリオシーン作り終えました。ついでにタイトルのスタートボタンを押したらシナリオシーンへ飛ぶようにしました。

---
+++ Assets/JOKER/Scripts/Novel/Core/SaveManager.cs
@@ -0,0 +1,407 @@
+﻿
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO.Compression;
+using System.Text;
+//using MiniJSON;
+
+
+namespace Novel
+{
+
+	//ゲーム全体に関する情報を保持する
+	[Serializable]
+	public class SaveGlobalObject{
+
+		//global変数を保持する。ゲームごとに変わらない変数 global.x みたいなやつ
+		public Dictionary<string,string> globalVar = new Dictionary<string,string>() ;
+
+
+	}
+
+
+	[Serializable]
+	public class SaveObject{
+
+		public string currentFile ="";
+		public int  currentIndex =-1;
+
+		public string name ="";
+		public string title ="";
+		public string description ="";
+		public string date = "";
+		public string currentMessage ="";
+
+		public bool visibleMessageFrame = true;
+		public bool enableNextOrder =true;
+		public bool enableEventClick = true;
+		public bool enableClickOrder = true;
+		public string currentPlayBgm = "";
+
+		public bool isEventStop = false;
+
+		//画面のキャプチャ情報
+		public string cap_img_file ="";
+
+		//ImageManager 編
+		//dicImage
+		public Dictionary <string,Image> dicImage ;
+		//dicTab 
+		public Dictionary<string,Dictionary<string,Image>> dicTag ;
+
+		//イベント管理用
+		public Dictionary<string,EventObject> dicEvent ; 
+
+		//スタック管理
+		public ScenarioManager scenarioManager;
+
+		public LogManager logManager;
+
+		//変数管理　
+		public Variable variable;
+
+
+	}
+
+	public class SaveManager
+	{
+
+		private string storagePath ;
+
+
+		public SaveManager ()
+		{
+
+			this.storagePath = Application.persistentDataPath + "/novel";
+
+		}
+
+		//グローバルセッティングを保存します
+		public void saveGlobal(SaveGlobalObject sobj){
+
+			NovelSingleton.GameManager.globalSetting = sobj;
+			string json = LitJson.JsonMapper.ToJson (sobj);
+
+
+			//WebPlayer の場合保存方法に変化を入れる
+			if (Application.platform == RuntimePlatform.WindowsWebPlayer || Application.platform == RuntimePlatform.OSXWebPlayer) {
+
+				PlayerPrefs.SetString("setting.dat", json);
+
+			} else {
+
+				string path = storagePath + "/setting.dat";
+
+				//ディレクトリ存在チェック
+				if (!Directory.Exists (storagePath)) {
+					Directory.CreateDirectory (storagePath);
+				}
+
+
+				FileStream fs = new FileStream (path,
+					               FileMode.Create,
+					               FileAccess.Write);
+
+				StreamWriter sw = new StreamWriter (fs);
+				sw.Write (json);
+				sw.Flush ();
+				sw.Close ();
+				fs.Close ();
+			}
+		}
+
+		public void loadGlobal(){
+
+			//WebPlayer の場合保存方法に変化を入れる
+			if (Application.platform == RuntimePlatform.WindowsWebPlayer || Application.platform == RuntimePlatform.OSXWebPlayer) {
+
+				if (!PlayerPrefs.HasKey ("setting.dat")) {
+					this.saveGlobal (new SaveGlobalObject ());
+				}
+
+				string json = PlayerPrefs.GetString ("setting.dat");
+
+				SaveGlobalObject obj = LitJson.JsonMapper.ToObject<SaveGlobalObject> (json);
+				NovelSingleton.GameManager.globalSetting = obj;
+
+
+			} else {
+
+
+				string path = storagePath + "/setting.dat";
+
+				if (!File.Exists (path)) {
+					//ファイル作製
+					this.saveGlobal (new SaveGlobalObject ());
+				}
+
+				FileStream fs = new FileStream (path,
+					               FileMode.Open,
+					               FileAccess.Read);
+
+				StreamReader sr = new StreamReader (path, System.Text.Encoding.Default);
+				string json = sr.ReadToEnd ();
+
+				SaveGlobalObject obj = LitJson.JsonMapper.ToObject<SaveGlobalObject> (json);
+
+				sr.Close ();
+				fs.Close ();
+
+				NovelSingleton.GameManager.globalSetting = obj;
+
+			}
+
+			//グローバル変数を格納する
+			StatusManager.variable.replaceAll("global",NovelSingleton.GameManager.globalSetting.globalVar);
+
+			StatusManager.variable.trace ("global");
+
+		}
+
+		//一時退避しておいたスナップから保存を実行する
+		public void saveFromSnap(string name){
+		
+			//一時領域からデータ取得
+			string path = storagePath + "/savesnap.sav";
+			object obj = LoadFromBinaryFile (path);
+			if (obj == null) {
+			
+			} else {
+				SaveObject sobj = (SaveObject)LoadFromBinaryFile (path);
+				string w_path = storagePath + "/"+name+".sav";
+				SaveToBinaryFile(sobj, w_path);
+			}
+
+		}
+
+		//plus が true の場合は、一つ進めたところをロードさせる。sleepgameの後とか戻ってきた時用
+		public void save(string save_name,bool plus =false){
+
+			SaveObject sobj = new SaveObject ();
+			sobj.name = save_name;
+			//タイトルとか、基本情報を格納
+			sobj.title = NovelSingleton.GameManager.scene.messageForSaveTitle;
+			sobj.date = DateTime.Now.ToString ("yyyy/MM/dd HH:mm:ss");
+			sobj.currentMessage = NovelSingleton.GameManager.scene.messageForSaveTitle;
+
+			sobj.dicImage = NovelSingleton.GameManager.imageManager.dicImage;
+			sobj.dicTag   = NovelSingleton.GameManager.imageManager.dicTag;
+			sobj.dicEvent = NovelSingleton.GameManager.eventManager.dicEvent;
+			sobj.scenarioManager = NovelSingleton.GameManager.scenarioManager;
+			sobj.variable = StatusManager.variable;
+			sobj.currentFile = StatusManager.currentScenario;
+			sobj.currentIndex = NovelSingleton.GameManager.CurrentComponentIndex;
+			sobj.logManager = NovelSingleton.GameManager.logManager;
+
+			//ステータス
+			sobj.visibleMessageFrame = StatusManager.visibleMessageFrame;
+			sobj.enableNextOrder = StatusManager.enableNextOrder;
+			sobj.enableEventClick = StatusManager.enableEventClick;
+			sobj.enableClickOrder = StatusManager.enableClickOrder;
+			sobj.currentPlayBgm = StatusManager.currentPlayBgm;
+			sobj.isEventStop = StatusManager.isEventStop;
+
+			//画面のキャプチャを作成して保存する
+			//保存先のパス
+
+			if (plus == true) {
+				sobj.currentIndex++;
+			}
+
+			//sobjをシリアライズ化して保存 
+
+			string path = storagePath + "/"+save_name+".sav";
+
+			SaveToBinaryFile(sobj, path);
+
+		}
+
+		public void SaveToBinaryFile(SaveObject obj, string path)
+		{
+
+			string json = LitJson.JsonMapper.ToJson (obj);
+
+			//WebPlayer の場合保存方法に変化を入れる
+			if (Application.platform == RuntimePlatform.WindowsWebPlayer || Application.platform == RuntimePlatform.OSXWebPlayer) {
+
+				PlayerPrefs.SetString(path, json);
+
+			} else {
+
+
+				if (!Directory.Exists(storagePath))
+				{
+					Directory.CreateDirectory(storagePath);
+				}
+
+				FileStream fs = new FileStream(path,
+					FileMode.Create,
+					FileAccess.Write);
+
+				StreamWriter sw = new StreamWriter(fs);
+				sw.Write(json);
+				sw.Flush ();
+				sw.Close ();
+				fs.Close();
+
+			}
+		}
+
+		public object LoadFromBinaryFile(string path)
+		{
+
+			//WebPlayer の場合保存方法に変化を入れる
+			if (Application.platform == RuntimePlatform.WindowsWebPlayer || Application.platform == RuntimePlatform.OSXWebPlayer) {
+
+				string json = PlayerPrefs.GetString (path);
+				SaveObject obj = LitJson.JsonMapper.ToObject<SaveObject> (json);
+
+				return obj;
+
+			} else {
+
+				if (!File.Exists (path)) {
+					return null;
+				}
+
+				FileStream fs = new FileStream (path,
+					               FileMode.Open,
+					               FileAccess.Read);
+
+				StreamReader sr = new StreamReader (path, System.Text.Encoding.Default);
+				string json = sr.ReadToEnd ();
+
+				if (json == "") {
+					return null;
+				}
+
+				SaveObject obj = LitJson.JsonMapper.ToObject<SaveObject> (json);
+
+				sr.Close ();
+				fs.Close ();
+
+				return obj;
+
+			}
+
+
+		}
+
+		public SaveObject getSaveData(string data_name){
+		
+			string path = storagePath + "/"+data_name+".sav";
+
+			SaveObject obj = (SaveObject)LoadFromBinaryFile (path);
+
+			return obj;
+		
+		}
+
+		public void applySaveVariable(string save_name, string var_name="save"){
+
+			//最初のセーブデータを取得するか。
+			SaveObject sobj =  NovelSingleton.SaveManager.getSaveData(save_name);
+			//this.gameManager.saveManager.getSaveData ("save_"+current_index);
+			StatusManager.variable.set (var_name+".name", save_name);
+
+			if (sobj != null) {
+
+				StatusManager.variable.set (var_name+".title", sobj.title);
+				StatusManager.variable.set (var_name+".date", sobj.date);
+				StatusManager.variable.set (var_name+".description", sobj.description);
+				StatusManager.variable.set (var_name+".name", save_name);
+				//StatusManager.variable.set ("save.img", sobj.cap_img_file);
+
+			} else {
+
+				StatusManager.variable.set (var_name+".title", "データがありません");
+				StatusManager.variable.set (var_name+".date", "" );
+				StatusManager.variable.set (var_name+".description", "");
+				StatusManager.variable.set (var_name+".name", save_name);
+				//StatusManager.variable.set ("save.img", "");
+
+			}
+
+
+		}
+
+		/*		
+
+		public string Base64FromStringComp(string st)
+		{
+			// 文字列をバイト配列に変換します
+			byte[] source = Encoding.UTF8.GetBytes(st);
+			return this._Base64FromStringComp(source);
+
+		}
+
+		public string _Base64FromStringComp(byte[] source)
+		{
+
+
+
+			// 入出力用のストリームを生成します
+			MemoryStream ms = new MemoryStream();
+			DeflateStream CompressedStream = new DeflateStream(ms, CompressionMode.Compress);
+
+			// ストリームに圧縮するデータを書き込みます
+			CompressedStream.Write(source, 0, source.Length);
+			CompressedStream.Close();
+
+			// 圧縮されたデータを バイト配列で取得します
+			byte[] destination = ms.ToArray();
+
+			//Base64で文字列に変換
+			string base64String;
+			base64String = System.Convert.ToBase64String(destination, Base64FormattingOptions.InsertLineBreaks);
+
+			return base64String;
+		}
+
+//------------------------------------------
+//BASE64文字列を戻し解凍の上で文字列に変換して返す
+//
+//------------------------------------------
+		public string StringFromBase64Comp(string st)
+		{
+			Debug.Log (st);
+			#region BASE64文字列を圧縮バイナリへ戻す
+			byte [] bs = System.Convert.FromBase64String(st);
+			#endregion
+
+			#region 圧縮バイナリを文字列へ解凍する
+
+			// 入出力用のストリームを生成します
+			MemoryStream ms = new MemoryStream(bs);
+			MemoryStream ms2 = new MemoryStream();
+
+			DeflateStream CompressedStream = new DeflateStream(ms, CompressionMode.Decompress);
+
+			//　MemoryStream に展開します
+			while (true)
+			{
+				int rb = CompressedStream.ReadByte();
+				// 読み終わったとき while 処理を抜けます
+				if (rb == -1)
+				{
+					break;
+				}
+				// メモリに展開したデータを読み込みます
+				ms2.WriteByte((byte)rb);
+			}
+
+			string result = Encoding.UTF8.GetString(ms2.ToArray());
+			#endregion
+
+			return result;
+		}
+		*/
+
+	}
+
+}
---
+++ Assets/JOKER/Scripts/Novel/Core/ScenarioManager.cs
@@ -0,0 +1,325 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+
+namespace Novel{
+
+	[Serializable]
+	public class Scenario
+	{
+		public string name;
+		public List<AbstractComponent> arrayComponent;
+		public Dictionary<string,int> dicLabel = new Dictionary<string,int>();
+
+		public Scenario(){
+		}
+
+		public Scenario (string scenario_name, List<AbstractComponent> list)
+		{
+			this.name = scenario_name;
+			this.arrayComponent = list;
+		}
+
+		public void addLabel(string label_name,int index){
+			//Debug.Log (label_name);
+			this.dicLabel [label_name] = index;
+		}
+
+		public int getIndex(string label_name){
+
+			if (label_name == "")
+				return -1;
+
+			if (!this.dicLabel.ContainsKey (label_name)) {
+				NovelSingleton.GameManager.showError (this.name+"にラベル「"+label_name+"」が見つかりません。");
+			}
+
+			return this.dicLabel[label_name];
+
+		}
+
+	}
+
+	[Serializable]
+	public class Macro
+	{
+		public string name;
+		public string file_name;
+		public int index;
+
+		public Macro(){
+		}
+
+		public Macro (string name, string file_name,int index)
+		{
+			this.name = name;
+			this.file_name = file_name;
+			this.index = index;
+		}
+
+	}
+
+	//マクロ呼出しを保持するクラス
+	[Serializable]
+	public class CallStack {
+
+		public Dictionary<string,string> dicVar = new Dictionary<string,string>();
+		public string scenarioNname;
+		public int index;
+
+		public CallStack(){
+		}
+
+		public CallStack(string scenario_name,int index,Dictionary<string,string> dicVar){
+			this.scenarioNname = scenario_name;
+			this.index = index;
+			this.dicVar = dicVar;
+		}
+
+
+	}
+
+	//if文の入れ子などを管理するスタック
+	[Serializable]
+	public class IfStack{
+
+		public bool isIfProcess = false;
+
+		public IfStack(){
+		}
+
+		public IfStack(bool val){
+			this.isIfProcess = val;
+		}
+
+
+	}
+
+	[Serializable]
+	public class ScenarioManager  {
+
+		[NonSerialized]
+		private Dictionary<string, Scenario> dicScenario = new Dictionary<string,Scenario>();
+
+		public Dictionary<string, Macro> dicMacro = new Dictionary<string,Macro> ();
+
+		//stackを配列に置き換える
+		//public Stack<CallStack> qStack = new Stack<CallStack>();
+		//public Stack<IfStack> ifStack = new Stack<IfStack>();
+
+		public List<CallStack> qStack = new List<CallStack>();
+		public List<IfStack> ifStack = new List<IfStack>();
+
+
+		public int ifNum = 0;
+		public int macroNum = 0;
+
+		public ScenarioManager(){
+
+		}
+
+		public Scenario getScenario(string file_name){
+
+			//Debug.Log ("=  getScenario  =======================");
+			//Debug.Log (file_name);
+			//Debug.Log (this.dicScenario.ContainsKey (file_name));
+
+			//シナリオからロードしてきた時はnull になってるからね
+			if (this.dicScenario == null) {
+				this.dicScenario = new Dictionary<string,Scenario>();
+			}
+
+			if (this.dicScenario.ContainsKey (file_name)) {
+
+				return this.dicScenario [file_name];
+			} else {
+				return null;
+			}
+
+		}
+
+		//シナリオの追加 ラベルの位置計算もここでやる
+		public void addScenario(string scenario_name,List<AbstractComponent> list){
+
+			this.dicScenario [scenario_name] = new Scenario (scenario_name,list);
+
+			int index = 0;
+			foreach(AbstractComponent cmp in list){
+				if (cmp.tagName == "label") {
+					this.dicScenario [scenario_name].addLabel(cmp.originalParam["name"],index);
+				}
+
+				index++;
+			}
+		
+		}
+
+
+		public int getIndex(string scenario_name,string label_name){
+
+			//シナリオがまだ読み込まれていない場合は読み込みを行う
+			if (!this.dicScenario.ContainsKey (scenario_name)) {
+				return -1;
+			}
+
+			return this.dicScenario [scenario_name].getIndex (label_name);
+
+		}
+
+		public void addStack(string scenario_name,int index,Dictionary<string,string> dicVar){
+
+			//stack追加時にdicVarに呼び出し元情報を入れる
+			//呼び出し元の情報はcaller_indexに入る。
+			dicVar ["caller_index"] = ""+index;
+			dicVar ["caller_file"] = scenario_name;
+
+			var mp = StatusManager.variable.getType ("mp");
+
+			////
+			/*
+			Debug.Log ("== add stack ===============");
+			foreach (KeyValuePair<string, string> kvp in mp){
+				Debug.Log (kvp.Key);
+				Debug.Log(kvp.Value);
+			}
+			*/
+
+			this.qStack.Add (new CallStack(scenario_name,index,mp));
+
+			//スタックを追加した時点で使用できる引数変数を格納する
+			StatusManager.variable.replaceAll("mp",dicVar);;
+
+		}
+
+		public CallStack popStack(){
+
+			try{
+
+				CallStack c = this.qStack[this.qStack.Count-1];
+
+				//var mp = StatusManager.variable.getType ("mp");
+
+				/*
+				Debug.Log ("== pop stack ===============");
+				foreach (KeyValuePair<string, string> kvp in c.dicVar){
+					Debug.Log (kvp.Key);
+					Debug.Log(kvp.Value);
+				}
+				*/
+
+
+
+				StatusManager.variable.replaceAll("mp",c.dicVar);;
+
+				this.qStack.RemoveAt(this.qStack.Count-1);
+
+				return c;
+
+			}catch(System.Exception e){
+				NovelSingleton.GameManager.showError ("スタックが不足しています。callとreturnの関係を確認して下さい");
+				Debug.Log (e.ToString ());
+				return null;
+			}
+		}
+
+		public int countStack(){
+			return this.qStack.Count;
+		}
+
+
+		/// <summary>
+		/// //////if 周りのスタック管理
+		/// </summary>
+
+		public void addIfStack(bool proccess){
+
+			this.ifStack.Add (new IfStack(proccess));
+
+		}
+
+		public IfStack popIfStack(){
+
+			try{
+
+				IfStack c = this.ifStack[this.ifStack.Count-1];
+				this.ifStack.RemoveAt(this.ifStack.Count-1);
+
+				return c;
+
+			}catch(System.Exception e){
+				Debug.Log (e.ToString ());
+				NovelSingleton.GameManager.showError ("スタックが不足しています。ifとendifの関係を確認して下さい");
+				return null;
+			}
+		}
+
+		//現在のifスタックの状態を確認する
+		public bool currentIfStack(){
+
+			return this.ifStack[this.ifStack.Count-1].isIfProcess;
+		
+		}
+
+		//スタックの状態を変更する
+		public void changeIfStack(bool proccess){
+
+			IfStack s = this.popIfStack();
+			s.isIfProcess = proccess;
+			this.ifStack.Add (s);
+
+		}
+
+		public int countIfStack(){
+			return this.ifStack.Count;
+		}
+
+		//スタックをすべて削除します
+
+		public void removeAllStacks(){
+			//未実装
+			this.qStack.Clear ();
+			this.ifStack.Clear ();
+			this.ifNum = 0;
+
+		}
+
+
+
+//// macro ///////
+
+		public void addMacro(string macro_name,string file_name, int index){
+
+			this.dicMacro[macro_name] = new Macro(macro_name,file_name,index);
+
+		}
+
+		/*
+		public void addMacroStack(string macro_name,Dictionary<string,string> dicVar){
+			this.macroNum++;
+			Macro macro = dicMacro [macro_name];
+			this.addStack (macro.file_name, macro.index, dicVar);
+
+		}
+		*/
+
+		public Macro getMacro(string macro_name){
+
+			if(!this.dicMacro.ContainsKey (macro_name)){
+				NovelSingleton.GameManager.showError ("マクロ「" + macro_name + "」は見つかりませんでした");
+			}
+
+			return this.dicMacro [macro_name]; 
+
+		}
+
+
+
+
+
+
+	}
+
+
+}

e10d7b3a84fcf696935d8bddced87843951a9e3c
nojima <17d3170020nm@edu.tech.ac.jp>
2018-07-03 00:43:59 +0900

    ダメージを受けた時、与えた時にParty、Enemyの下にtextでダメージの表示

--- Assets/menber/nojima/scripts/HP/hp.cs
+++ Assets/menber/nojima/scripts/HP/hp.cs
@@ -6,24 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class hp : MonoBehaviour
 {
+
+    public GameObject PartyDamage;
+    public GameObject EnemyDamage;
+
+    public Text PD;
+    public Text ED;
+
     Slider _enemyslider;
     Slider _partyslider;
     // Use this for initialization
     void Start()
     {
+
+        PartyDamage.SetActive(false);
+        EnemyDamage.SetActive(false);
         //スライダーを取得
         _enemyslider = GameObject.Find("enemySlider").GetComponent<Slider>();
 
         _partyslider = GameObject.Find("partySlider").GetComponent<Slider>();
     }
     //HPの値
-    public  float partyhp = 200;
-    public  float enemyhp = 800;
+    public float partyhp = 200;
+    public float enemyhp = 800;
     //HPを減らす
     // Update is called once per frame
     void Update()
     {
-        
+
         //HPゲージに値を設定
         _enemyslider.value = enemyhp;
 
@@ -37,20 +47,32 @@ public class hp : MonoBehaviour
             SceneManager.LoadScene("Win");
         }
     }
-    public void DownPartyHp(bool i,int j)
+    public void DownPartyHp(bool i, int j)
     {
         //防御成功：味方のHPを敵の攻撃力の20％分減らす
-        if(i == true){
+        if (i == true)
+        {
             float k = j * 0.2f;
             partyhp -= k;
+            EnemyDamage.SetActive(true);
+            string X = k.ToString();
+            ED.text = X;
         }
-        else {
+        else
+        {
             //防御失敗：味方のHPを敵の攻撃力分減らす
             partyhp -= j;
+            EnemyDamage.SetActive(true);
+            string Y = j.ToString();
+            ED.text = Y;
         }
 
     }
-    public void DownEnemyHp(int i){
+    public void DownEnemyHp(int i)
+    {
         enemyhp -= i;
+        PartyDamage.SetActive(true);
+        string Z = i.ToString();
+        PD.text = Z;
     }
 }

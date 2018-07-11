a1030ce77ec47ea880f6c7ea79ec8b5cec8caa25
野島 <17d3170020nm@edu.tech.ac.jp>
2018-07-11 18:51:00 +0900

    ダメージ表示時にtextが跳ねるよう（作成途中）

--- Assets/menber/nojima/scripts/HP/hp.cs
+++ Assets/menber/nojima/scripts/HP/hp.cs
@@ -15,9 +15,17 @@ public class hp : MonoBehaviour
 
     Slider _enemyslider;
     Slider _partyslider;
+
+    bool EDcount;
+    bool PDcount;
+
+    float TimeCountX = 1;
+    float TimeCountZ = 1;
     // Use this for initialization
-    void Start()
-    {
+    void Start(){
+        EDcount = false;
+        PDcount = false;
+
 
         PartyDamage.SetActive(false);
         EnemyDamage.SetActive(false);
@@ -31,48 +39,59 @@ public class hp : MonoBehaviour
     public float enemyhp = 800;
     //HPを減らす
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
 
+        if (EDcount == true) {
+            TimeCountX -= Time.deltaTime;
+        }
+        if (PDcount == true){
+            TimeCountZ -= Time.deltaTime;
+        }
         //HPゲージに値を設定
         _enemyslider.value = enemyhp;
 
         _partyslider.value = partyhp;
-        if (partyhp <= 0)
-        {
+        if (partyhp <= 0){
             SceneManager.LoadScene("GameOver");
         }
-        if (enemyhp <= 0)
-        {
+        if (enemyhp <= 0){
             SceneManager.LoadScene("Win");
         }
     }
-    public void DownPartyHp(bool i, int j)
-    {
+    public void DownPartyHp(bool i, int j){
         //防御成功：味方のHPを敵の攻撃力の20％分減らす
-        if (i == true)
-        {
+        if (i == true){
             float k = j * 0.2f;
             partyhp -= k;
             EnemyDamage.SetActive(true);
             string X = k.ToString();
             ED.text = X;
+            EDcount = true;
+            if (TimeCountX == 0) {
+                EnemyDamage.SetActive(false);
+            }
         }
-        else
-        {
+        else{
             //防御失敗：味方のHPを敵の攻撃力分減らす
             partyhp -= j;
             EnemyDamage.SetActive(true);
             string Y = j.ToString();
             ED.text = Y;
+            EDcount = true;
+            if (TimeCountX == 0) {
+                EnemyDamage.SetActive(false);
+            }
         }
 
     }
-    public void DownEnemyHp(int i)
-    {
+    public void DownEnemyHp(int i){
         enemyhp -= i;
         PartyDamage.SetActive(true);
         string Z = i.ToString();
         PD.text = Z;
+        PDcount = true;
+        if (TimeCountZ == 0){
+            PartyDamage.SetActive(false);
+        }
     }
 }

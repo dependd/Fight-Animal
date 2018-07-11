b1b929dacd3cec5c33a8adb86e75f03d17782b5b
高田将志 <masapo.0315@icloud.com>
2018-07-11 17:35:00 +0900

    ステージセレクトのシーンを編集した。バトルに勝利したら一度ステージセレクトに移動するようにした。

---
+++ Assets/menber/takada/StageSerect/OpenMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenMenu : MonoBehaviour {
+
+    public GameObject MenuScreen;
+
+    private void Start()
+    {
+        MenuScreen.SetActive(false);
+
+    }
+
+    //ボタンがクリックされたらパネルを表示
+    public void OnClick()
+    {
+        MenuScreen.SetActive(true);
+    }
+
+}

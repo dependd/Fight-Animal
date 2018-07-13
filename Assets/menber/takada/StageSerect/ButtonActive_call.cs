54ad68cda702a57788713d334d8bad77aa8b21c5
高田将志 <masapo.0315@icloud.com>
2018-07-14 00:32:44 +0900

    ステージセレクトの製作途中

---
+++ Assets/menber/takada/StageSerect/ButtonActive_call.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActive_call : MonoBehaviour {
+
+    public GameObject BattleButton1;
+    public GameObject BattleButton2;
+
+    public void TutorialWinLoad(){
+
+        BattleButton1.SetActive(true);
+
+    }
+
+    public void Battle1winLoad(){
+
+        BattleButton2.SetActive(true);
+
+    }
+
+}

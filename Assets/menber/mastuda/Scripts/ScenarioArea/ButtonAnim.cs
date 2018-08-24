b9b95772251c6a81bfe4be3dc06f9bfe2365e207
松田幸介 <darby8928@gmail.com>
2018-08-24 17:58:47 +0900

    ボタンの強調の処理を変更した

--- Assets/menber/mastuda/Scripts/ScenarioArea/ButtonAnim.cs
+++ Assets/menber/mastuda/Scripts/ScenarioArea/ButtonAnim.cs
@@ -4,17 +4,72 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonAnim : MonoBehaviour {
-    
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
 
-    private void ButtonEnlargement()
+    [SerializeField] private float mMaxScale = 1.1f;
+    [SerializeField] private float mTimer = 0.5f;
+
+    private RectTransform mRectTransform;
+    private bool mIsScaleUp = false;
+    private bool mIsAction = false;
+
+    private void Start()
     {
-       
+        mRectTransform = GetComponent<RectTransform>();
+        mIsAction = true;
+        ScaleUp();
+    }
+
+    public void StartAction()
+    {
+        mIsAction = true;
+        ScaleUp();
+    }
+    public void StopAction()
+    {
+        StartCoroutine(ScaleUpAction());
+    }
+    private void ScaleUp()
+    {
+        StartCoroutine(ScaleUpAction());
+    }
+    IEnumerator ScaleUpAction()
+    {
+        var localScale = mRectTransform.localScale;
+        var scale = localScale.x;
+        var time = 0f;
+        while(time < 1)
+        {
+            if (!mIsAction) yield break;
+            time += (1 / mTimer) * Time.deltaTime;
+            if (time >= 1f) time = 1f;
+            scale = Mathf.Lerp(1, mMaxScale, time);
+            localScale.x = scale;
+            localScale.y = scale;
+            mRectTransform.localScale = localScale;
+            yield return null;
+        }
+        if (mIsAction) ScaleDown();
+    }
+    private void ScaleDown()
+    {
+        StartCoroutine(ScaleDownAction());
+    }
+    IEnumerator ScaleDownAction()
+    {
+        var localScale = mRectTransform.localScale;
+        var scale = localScale.x;
+        var time = 0f;
+        while (time < 1)
+        {
+            if (!mIsAction) yield break;
+            time += (1 / mTimer) * Time.deltaTime;
+            if (time >= 1f) time = 1f;
+            scale = Mathf.Lerp(1, mMaxScale, 1 - time);
+            localScale.x = scale;
+            localScale.y = scale;
+            mRectTransform.localScale = localScale;
+            yield return null;
+        }
+        if (mIsAction) ScaleUp();
     }
 }

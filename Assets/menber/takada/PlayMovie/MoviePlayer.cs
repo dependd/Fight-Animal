6eeaa2fb8a89935835f85eda74c60d173b5ee605
高田将志 <masapo.0315@icloud.com>
2018-09-11 00:24:47 +0900

    映像データ再生用のスクリプトを製作開始

---
+++ Assets/menber/takada/PlayMovie/MoviePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class MoviePlayer : MonoBehaviour {
+
+    // VideoPlayerのコンポーネント
+    private VideoPlayer moviePlayer;
+    // AudioSourceのコンポーネント
+    private AudioSource audioSource;
+    // 内部に保存したテクスチャを表示する
+    public RawImage rawImage;
+
+	// Use this for initialization
+	void Start () {
+
+        moviePlayer = GetComponent<VideoPlayer>();
+        // スクリプトでAudioOutputModeをAudioSourceに変更
+        moviePlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        audioSource = GetComponent<AudioSource>();
+        //
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

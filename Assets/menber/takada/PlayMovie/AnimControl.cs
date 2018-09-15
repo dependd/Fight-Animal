d455b703f7d1f47ed01ae913a448fc4d084f5a70
高田将志 <masapo.0315@icloud.com>
2018-09-15 13:47:45 +0900

    チンパンの変身アニメーション追加

---
+++ Assets/menber/takada/PlayMovie/AnimControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour {
+
+
+    [SerializeField] private Animator _animator;
+
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            _animator.SetTrigger("AnimStart");
+        }
+	}
+}
